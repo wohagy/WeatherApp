2249d0c4ab2f4dc894e3b106ec3035ca0d4e5d85
Полина Шубина <polinasubina@MacBook-Pro.local>
2022-01-29 21:53:06 +0300

    first commit

---
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by wohagy on 29.01.2022.
+//  Copyright © 2022 wohagy. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
